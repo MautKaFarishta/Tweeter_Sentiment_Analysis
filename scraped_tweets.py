79911b7c15180ee8619555a962f6a5b1f7d96d15
Omkar Khilari <60102972+MautKaFarishta@users.noreply.github.com>
2021-05-09 19:54:31 +0530

    Update scraped_tweets.py

--- scraped_tweets.py
+++ scraped_tweets.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import tweepy 
 
-# function to display data of each tweet
 def printtweetdata(n, ith_tweet):
     print()
     print(f"Tweet {n}:")
@@ -16,27 +15,18 @@ def printtweetdata(n, ith_tweet):
     print(f"Hashtags Used:{ith_tweet[8]}")
   
   
-# function to perform data extraction
 def scrape(words, date_since, numtweet):
       
-    # Creating DataFrame using pandas
     db = pd.DataFrame(columns=['username', 'description', 'location', 'following',
                                'followers', 'totaltweets', 'retweetcount', 'text', 'hashtags'])
       
-    # We are using .Cursor() to search through twitter for the required tweets.
-    # The number of tweets can be restricted using .items(number of tweets)
     tweets = tweepy.Cursor(api.search, q=words, lang="en",
                            since=date_since, tweet_mode='extended').items(numtweet)
      
-    # .Cursor() returns an iterable object. Each item in 
-    # the iterator has various attributes that you can access to 
-    # get information about each tweet
     list_tweets = [tweet for tweet in tweets]
       
-    # Counter to maintain Tweet Count
     i = 1  
       
-    # we will iterate over each tweet in the list for extracting information about each tweet
     for tweet in list_tweets:
         username = tweet.user.screen_name
         description = tweet.user.description
@@ -48,8 +38,6 @@ def scrape(words, date_since, numtweet):
         retweetcount = tweet.retweet_count
         hashtags = tweet.entities['hashtags']
           
-        # Retweets can be distinguished by a retweeted_status attribute,
-        # in case it is an invalid reference, except block will be executed
         try:
             text = tweet.retweeted_status.full_text
         except AttributeError:
@@ -58,24 +46,19 @@ def scrape(words, date_since, numtweet):
         for j in range(0, len(hashtags)):
             hashtext.append(hashtags[j]['text'])
           
-        # Here we are appending all the extracted information in the DataFrame
         ith_tweet = [username, description, location, following,
                      followers, totaltweets, retweetcount, text, hashtext]
         db.loc[len(db)] = ith_tweet
           
-        # Function call to print tweet data on screen
         printtweetdata(i, ith_tweet)
         i = i+1
     filename = 'scraped_tweets.csv'
       
-    # we will save our database as a CSV file.
     db.to_csv(filename)
   
   
 if __name__ == '__main__':
-      
-    # Enter your own credentials obtained 
-    # from your developer account
+     
     consumer_key = 'XBOMTUDVy5b0zOdWqRWYJuGkw'
     consumer_secret = 'oYK2W4jiARg3NQSFBklWwXgg9vhz1raviEd6OiFsZ7UsAAia17'
     access_key = '2572669031-n1HzM9bPb00wp4ZSuHWBsd7Wn9AEEShg2Z5z8eg'
@@ -84,13 +67,11 @@ if __name__ == '__main__':
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
       
-    # Enter Hashtag and initial date
     print("Enter Twitter HashTag to search for")
     words = input()
     print("Enter Date since The Tweets are required in yyyy-mm--dd")
     date_since = input()
       
-    # number of tweets you want to extract in one run
     numtweet = 8000
     scrape(words, date_since, numtweet)
     print('Scraping has completed!')
