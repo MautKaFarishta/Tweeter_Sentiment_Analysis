871354269d9222aa31c305f8e8673614b9e3e3a1
Omkar Khilari <omkhilariindia@gmail.com>
2021-06-26 11:46:41 +0530

    paths

---
+++ path.py
@@ -0,0 +1,14 @@
+path1 = 'doge_to_moon.csv'
+path2 = 'UserTweets.csv'
+path3 = 'scraped_tweets.csv'
+URL_path = 'url.txt'
+rply_path = 'tweetreply.csv'
+rply_scrap_file = 'twitter-scraper.py'
+
+
+
+
+
+
+
+
